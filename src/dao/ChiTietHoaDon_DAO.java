241b4ce5f768dafbc4a9e67b7fab03821fbdcd47
Kha <minhkha20031996@gmail.com>
2023-05-07 16:46:58 +0700

    thaydoisql

--- src/dao/ChiTietHoaDon_DAO.java
+++ src/dao/ChiTietHoaDon_DAO.java
@@ -17,17 +17,20 @@ public class ChiTietHoaDon_DAO {
 	public List<ChiTietHoaDon> getAllChiTietHoaDon() {
 		ConnectDB.getInstance();
 		List<ChiTietHoaDon> dsChiTietHoaDon = new ArrayList<ChiTietHoaDon>();
-		ThongTinXe_DAO 
 		Connection connection = ConnectDB.getConnection();
+		ThongTinXe_DAO thongTinXe_DAO = new ThongTinXe_DAO();
+		ThongTinXe thongTinXe = new ThongTinXe();
 		try {
-			String sqlString ="select * from ChiTietHoaDon";
+			String sqlString = "select * from ChiTietHoaDon";
 			Statement statement = connection.createStatement();
 			ResultSet rsResultSet = statement.executeQuery(sqlString);
 			while (rsResultSet.next()) {
-				dsChiTietHoaDon.add(new ChiTietHoaDon(rsResultSet.getString(1), null ,Integer.valueOf(rsResultSet.getString(3)),Integer.valueOf(rsResultSet.getString(4))));
-				
+				thongTinXe = thongTinXe_DAO.getThongTinXeTheoMa(rsResultSet.getString(2));
+				dsChiTietHoaDon.add(new ChiTietHoaDon(rsResultSet.getString(1), thongTinXe,
+						Integer.valueOf(rsResultSet.getString(3)), Integer.valueOf(rsResultSet.getString(4))));
+
 			}
-		}catch (SQLException e) {
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		return dsChiTietHoaDon;
@@ -40,12 +43,49 @@ public class ChiTietHoaDon_DAO {
 		PreparedStatement stmPreparedStatement = null;
 		stmPreparedStatement = connection.prepareStatement("insert into ChiTietHoaDon values(?,?,?,?,?)");
 		stmPreparedStatement.setString(1, chiTietHoaDon.getMa());
-		stmPreparedStatement.setString(2, chiTietHoaDon.getTtXE().getMaLoai());
+		stmPreparedStatement.setString(2, chiTietHoaDon.getTtXE().getMaLoaiXe());
 		stmPreparedStatement.setInt(3, chiTietHoaDon.getSoLuong());
 		stmPreparedStatement.setInt(4, chiTietHoaDon.getDonGia());
 		stmPreparedStatement.setInt(5, chiTietHoaDon.getThanhTien());
 		stmPreparedStatement.close();
 		return stmPreparedStatement.executeUpdate() > 0;
+	}
+
+	// get chi tiết hóa đơn theo mã hóa đơn
+	public List<ChiTietHoaDon> getChiTietHoaDonTheoMa(String maString) throws SQLException {
+		List<ChiTietHoaDon> dsChiTietHoaDon = new ArrayList<ChiTietHoaDon>();
+		ConnectDB.getInstance();
+		Connection connection = ConnectDB.getConnection();
+		String sqlString = "SELECT * FROM [dbo].[ChiTietHoaDon] where maHoaDon = '"+maString+"'";
+
+		Statement statement = connection.createStatement();
+		ResultSet resultSet = statement.executeQuery(sqlString);
+		ThongTinXe_DAO thongTinXe_DAO = new ThongTinXe_DAO();
+		ThongTinXe thongTinXe = new ThongTinXe();
+		while (resultSet.next()) {
+			thongTinXe = thongTinXe_DAO.getThongTinXeTheoMa(resultSet.getString(2));
+			dsChiTietHoaDon.add(new ChiTietHoaDon(resultSet.getString(1), thongTinXe,
+					Integer.valueOf(resultSet.getString(3)), Integer.valueOf(resultSet.getString(4))));
+		}
+		return dsChiTietHoaDon;
+	}
+	// get theo mã loại xe
+	public List<ChiTietHoaDon> getChiTietHoaDonTheoMaLoaiXe(String maString) throws SQLException {
+		List<ChiTietHoaDon> dsChiTietHoaDon = new ArrayList<ChiTietHoaDon>();
+		ConnectDB.getInstance();
+		Connection connection = ConnectDB.getConnection();
+		String sqlString = "SELECT * FROM [dbo].[ChiTietHoaDon] where maLoaiXe = '"+maString+"'";
+
+		Statement statement = connection.createStatement();
+		ResultSet resultSet = statement.executeQuery(sqlString);
+		ThongTinXe_DAO thongTinXe_DAO = new ThongTinXe_DAO();
+		ThongTinXe thongTinXe = new ThongTinXe();
+		while (resultSet.next()) {
+			thongTinXe = thongTinXe_DAO.getThongTinXeTheoMa(resultSet.getString(2));
+			dsChiTietHoaDon.add(new ChiTietHoaDon(resultSet.getString(1), thongTinXe,
+					Integer.valueOf(resultSet.getString(3)), Integer.valueOf(resultSet.getString(4))));
+		}
+		return dsChiTietHoaDon;
 	}
 
 }
