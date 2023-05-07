77c17fa27cf5ce305fde73c06fb726925388bf6c
Kha <minhkha20031996@gmail.com>
2023-05-07 07:40:03 +0700

    f

--- src/gui/TrangChu_GUI.java
+++ src/gui/TrangChu_GUI.java
@@ -9,6 +9,7 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import java.awt.event.ActionListener;
+import java.sql.SQLException;
 import java.awt.event.ActionEvent;
 import javax.swing.JRadioButton;
 import javax.swing.JLabel;
@@ -69,11 +70,9 @@ public class TrangChu_GUI extends JFrame implements ActionListener{
 //				}
 //			}
 //		});
-<<<<<<< HEAD
-		new TrangChu_GUI().setVisible(true);
-=======
+
 		new TrangChu_GUI().setVisible(true);;
->>>>>>> 9736e031c4ebd5f1671315153d85dc86caa8e343
+
 	}
 
 	/**
@@ -110,11 +109,9 @@ public class TrangChu_GUI extends JFrame implements ActionListener{
 		menuBar.add(menuNhanVien);
 		
 		menuitemNhanVienKyThuat = new JMenuItem("Nhân viên kỹ thuật");
-<<<<<<< HEAD
+
 //		menuitemNhanVienKyThuat.setIcon(new ImageIcon(TrangChu_GUI.class.getResource("data//image//worker.png")));
-=======
-		//menuitemNhanVienKyThuat.setIcon(new ImageIcon(TrangChu_GUI.class.getResource("/image/worker.png")));
->>>>>>> 9736e031c4ebd5f1671315153d85dc86caa8e343
+
 		menuitemNhanVienKyThuat.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
@@ -123,11 +120,9 @@ public class TrangChu_GUI extends JFrame implements ActionListener{
 		menuNhanVien.add(menuitemNhanVienKyThuat);
 		
 		 menuitemNhanVienHanhChinh = new JMenuItem("Nhân viên hành chính");
-<<<<<<< HEAD
+
 //		 menuitemNhanVienHanhChinh.setIcon(new ImageIcon(TrangChu_GUI.class.getResource("data//image//employee.png")));
-=======
-		 //menuitemNhanVienHanhChinh.setIcon(new ImageIcon(TrangChu_GUI.class.getResource("/image/employee.png")));
->>>>>>> 9736e031c4ebd5f1671315153d85dc86caa8e343
+
 		menuNhanVien.add(menuitemNhanVienHanhChinh);
 		
 		JMenu menuHoaDon = new JMenu("Hóa đơn");
@@ -193,11 +188,9 @@ public class TrangChu_GUI extends JFrame implements ActionListener{
 		contentPane.setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("");
-<<<<<<< HEAD
+
 //		lblNewLabel.setIcon(new ImageIcon(TrangChu_GUI.class.getResource("/image/TrangChu.jpg")));
-=======
-		//lblNewLabel.setIcon(new ImageIcon(TrangChu_GUI.class.getResource("/image/TrangChu.jpg")));
->>>>>>> 9736e031c4ebd5f1671315153d85dc86caa8e343
+
 		lblNewLabel.setBounds(54, 123, 830, 476);
 		contentPane.add(lblNewLabel);
 		
@@ -238,7 +231,12 @@ public class TrangChu_GUI extends JFrame implements ActionListener{
 			showMenu(new NhanVienKyThuat_GUI());
 		}
 		else if(o.equals(menuitemHoaDon)) {
-			showMenu(new HoaDon_GUI());
+			try {
+				showMenu(new HoaDon_GUI());
+			} catch (SQLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
 		}
 		else if(o.equals(menuitemNhaPhanPhoi)) {
 			showMenu(new NhaPhanPhoi_GUI());
